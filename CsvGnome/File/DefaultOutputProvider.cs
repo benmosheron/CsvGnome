8eedc23e581e9881e36f09ee2a341b5da3519968
BenLaptop <revolverator@hotmail.com>
2016-10-22 16:29:54 +0100

    File namespace

---
+++ CsvGnome/File/DefaultOutputProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace CsvGnome.File
+{
+    public class DefaultOutputProvider : IProvider
+    {
+        const string Directory = "Output";
+        const string Name = "CsvGnomeOutput";
+        const string Extension = ".csv";
+
+        static DefaultOutputProvider staticInstance = new DefaultOutputProvider();
+
+        public static string DefaultPath => staticInstance.Path;
+
+        public string Path => System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), $@"{Directory}\{Name}.{Extension}");
+    }
+}
---
+++ CsvGnome/File/IProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvGnome.File
+{
+    /// <summary>
+    /// Provides file location, name and extension.
+    /// </summary>
+    public interface IProvider
+    {
+        /// <summary>
+        /// Path to the file. Including folder, name and extension.
+        /// </summary>
+        string Path { get; }
+    }
+}
