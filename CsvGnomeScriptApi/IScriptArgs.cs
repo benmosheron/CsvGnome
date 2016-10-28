9e8092bd1a81cfca424286d7e13a95f14c025208
Ben Sheron <benmosheron@gmail.com>
2016-10-28 19:28:31 +0100

    Removed unused references from CsvGnomeScriptApi.

--- CsvGnomeScriptApi/IScriptArgs.cs
+++ CsvGnomeScriptApi/IScriptArgs.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CsvGnomeScriptApi
+﻿namespace CsvGnomeScriptApi
 {
     /// <summary>
     /// Encompasses arguments that can be passed to script value generating functions.
